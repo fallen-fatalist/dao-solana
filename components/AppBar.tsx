f951e24f57bc343e2cb8cef1a8ea54143d98a68b
Murat <m_kairbek@outlook.com>
2024-06-11 20:23:01 +0500

    connection to wallet set up;

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 export const AppBar: FC = () => {
     return (
         <div className={styles.AppHeader}>
-            <Image src="/solanaLogo.png" height={30} width={200} />
+            <img src="/solanaLogo.png" height={30} width={200} />
             <span>Wallet-Adapter Example</span>
             <WalletMultiButton />
         </div>
