46a4ed52d82a8f695cfced2c2d3153d64b831387
Murat <m_kairbek@outlook.com>
2024-06-11 18:37:31 +0500

    begin with frontend template;

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,26 @@ import { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import WalletContextProvider from "../components/WalletContextProvider";
 import { AppBar } from "../components/AppBar";
-import { DisplayTokenInfo } from "../components/DisplayTokenInfo";
+import { BalanceDisplay } from "../components/BalanceDisplay";
+import { MintToForm } from "../components/MintToForm";
 import { CreateTokenAccountForm } from "../components/CreateTokenAccount";
+import { CreateMintForm } from "../components/CreateMint";
 import Head from "next/head";
 
 const Home: NextPage = (props) => {
   return (
     <div className={styles.App}>
       <Head>
-        <title>dao application</title>
-        <meta name="description" content="Decentralized Autonomous Organization App" />
+        <title>Token Program</title>
+        <meta name="description" content="Token Program" />
       </Head>
       <WalletContextProvider>
         <AppBar />
         <div className={styles.AppBody}>
-        <DisplayTokenInfo/>
+          <BalanceDisplay />
+          <CreateMintForm />
+          <CreateTokenAccountForm />
+          <MintToForm />
         </div>
       </WalletContextProvider>
     </div>
